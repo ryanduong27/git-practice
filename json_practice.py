e195a2a7fc66b49bb80b734538b3e7e1c10b16ed
Phuc <84716840+ryanduong27@users.noreply.github.com>
2021-06-07 11:00:50 +0700

    print age in json

--- json_practice.py
+++ json_practice.py
@@ -6,10 +6,5 @@ x = '{ "name":"John", "age":30, "city":"New York"}'
 # parse x:
 y = json.loads(x)
 
-# the result is a Python dictionary:
-<<<<<<< HEAD
-print(y["name"])
-print(x)
-=======
+# the result is a Python dictionary
 print(y["age"])
->>>>>>> 3879c18c64c4e5ea91330adf9f5ada154a953344
