a0238d61caf39446bea70460d46a56533fecbc4b
Phuc <84716840+ryanduong27@users.noreply.github.com>
2021-06-07 10:37:03 +0700

    Add python_advanced.py

---
+++ python_advanced.py
@@ -0,0 +1,14 @@
+def customize_data(func):
+    
+    def swapper(*args,**kwargs):
+        raw_data, file, useOption, style = func(*args, **kwargs)
+        print(raw_data, file, useOption, style)
+        return raw_data+1, file, useOption+2, style #semi-final
+    return swapper #final
+
+@customize_data
+def scroll_elk_data(raw_data, file, useOption, style):
+    raw_data += 1
+    return raw_data, file, useOption+1, style
+    
+scroll_elk_data(1,2,3,4)
