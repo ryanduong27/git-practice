96f759daefa87638781cd1d08b6b14156a252933
Phuc <84716840+ryanduong27@users.noreply.github.com>
2021-06-07 11:19:30 +0700

    test

---
+++ py_advanced.py
@@ -0,0 +1,16 @@
+def display_info(func):
+    def inner():
+        print(f"Excuting \"{func.__name__}\" function")
+        func()
+        print('Done!')
+    return inner
+
+@display_info
+def printer():
+    print("Hello World!")
+    
+@display_info
+def summer():
+    print("Too hot!")
+    
+printer()
